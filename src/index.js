1080aff74f3aa0f9bc90ba49d875536bd34d6619
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-18 22:35:42 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -152,12 +152,22 @@
  * 論理演算子の本当の意味を知ろう
  */
 
-const flag1 = true;
-const flag2 = false;
+// const flag1 = true;
+// const flag2 = false;
 
-if (flag1 || flag2) {
-  console.log("１か２はtrueになります");
-}
-if (flag1 && flag2) {
-  console.log("１も２はtrueになります");
-}
+// if (flag1 || flag2) {
+//   console.log("１か２はtrueになります");
+// }
+// if (flag1 && flag2) {
+//   console.log("１も２はtrueになります");
+// }
+
+// || は左側がfalseなら右側を返す
+// const num = null;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す
+// const num2 = null;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
