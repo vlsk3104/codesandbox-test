fa500c81b7423b3c307dce87f9c78c3b1c1bde15
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-18 18:51:09 +0900

    mapやfilterを使った配列の処理

--- src/index.js
+++ src/index.js
@@ -96,3 +96,35 @@
 // const arr8 = arr4;
 // arr8[0] = 100;
 // console.log(arr4);
+
+/**
+ * mapやfilterを使った配列の処理
+ */
+
+// const nameArr = ["田中", "山田", "じゅがる"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}です`);
+// }
+
+// const nameArr2 = nameArr.map((name) => {
+//   return name;
+// });
+// console.log(nameArr2);
+
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`));
+
+//配列の中である条件をして抽出する時に使う
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "じゅがる"){
+//     return name
+//   }else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
