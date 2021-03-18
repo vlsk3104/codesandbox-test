8db31180879d057d2d5a29a4861f0038c8110126
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-18 22:25:03 +0900

    三項演算子

--- src/index.js
+++ src/index.js
@@ -128,3 +128,36 @@
 //   }
 // })
 // console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+//ある条件？　条件がtrueの時　: 条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
+
+// const num = "1300";
+// console.log(num.toLocaleString());
+
+// const formattedNum =
+//   typeof num === "number" ? num.toLocaleString() : "数値を入力してください。";
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています' : '許容範囲です';
+// }
+// console.log(checkSum(50, 40));
+
+/**
+ * 論理演算子の本当の意味を知ろう
+ */
+
+const flag1 = true;
+const flag2 = false;
+
+if (flag1 || flag2) {
+  console.log("１か２はtrueになります");
+}
+if (flag1 && flag2) {
+  console.log("１も２はtrueになります");
+}
