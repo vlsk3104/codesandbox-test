2bb35f2a6e3a4b4dceaad60f957ee0406c3b1561
kuro <70840151+vlsk3104@users.noreply.github.com>
2021-03-18 12:23:57 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import "./styles.css";
+/**
+ * const,let等の変数宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var変数";
+
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可
+// let val2 = "let変数を再宣言";
+
+// const val3 = "const変数";
+// console.log(val3);
+
+//const変数は上書き不可
+//val3 = "const変数を上書き";
+
+//const val3 = "const変数を再宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゅがる",
+//   age: 27,
+// };
+// val4.name = "jugal";
+// val4.addrres = "tokyo";
+// console.log(val4);
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val5 = ['dog', 'cat'];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
